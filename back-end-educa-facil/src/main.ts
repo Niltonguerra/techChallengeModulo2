1b5dd3ce924269dcfeacf4f72e73fc4814698cbc
Niltonguerra <niltondg.39@gmail.com>
2025-06-13 15:21:08 -0300

    feat:melhoria no armazenamento dos dados do banco de dados

--- back-end-educa-facil/src/main.ts
+++ back-end-educa-facil/src/main.ts
@@ -1,10 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-require-imports
 (global as any).crypto = require('crypto');
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // // Configuração do microservice RabbitMQ
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.RMQ,
+  //   options: {
+  //     urls: ['amqp://guest:guest@rabbitmq:5672'],
+  //     queue: 'fila_teste', // nome da fila
+  //     queueOptions: {
+  //       durable: false,
+  //     },
+  //   },
+  // });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
