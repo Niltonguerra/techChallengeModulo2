6dbd774eaeb1e8028c7e7bc11f214a6a69955de6
Niltonguerra <niltondg.39@gmail.com>
2025-06-21 17:57:51 -0300

    fix:remoção da validação da branch

--- back-end-educa-facil/src/app.module.ts
+++ back-end-educa-facil/src/app.module.ts
@@ -3,21 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PostModule } from '@modules/post/post.module';
 import { Post } from '@modules/post/entities/post.entity';
-import gitModule from 'git-rev-sync';
 
-let env: string;
-
-try {
-  const git = gitModule as { branch: () => string };
-  const branch = git.branch();
-  if (branch === 'main' || branch === 'master') {
-    env = 'PROD';
-  } else {
-    env = 'DEV';
-  }
-} catch {
-  env = 'DEV';
-}
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -29,16 +15,15 @@ try {
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
         host:
-          config.get<string>('AMBIENTE') === 'PROD' && env === 'PROD'
+          config.get<string>('AMBIENTE') === 'PROD'
             ? config.get<string>('DB_HOST_PROD')
             : config.get<string>('DB_HOST_DEV'),
         port: parseInt(config.get<string>('DB_PORT') ?? '5432', 10),
         username: config.get<string>('DB_USERNAME'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_DATABASE'),
-        autoLoadEntities:
-          config.get<string>('AMBIENTE') === 'PROD' && env === 'PROD' ? false : true,
-        synchronize: config.get<string>('AMBIENTE') === 'PROD' && env === 'PROD' ? false : true,
+        autoLoadEntities: config.get<string>('AMBIENTE') === 'PROD' ? false : true,
+        synchronize: config.get<string>('AMBIENTE') === 'PROD' ? false : true,
         entities: [Post],
       }),
     }),
