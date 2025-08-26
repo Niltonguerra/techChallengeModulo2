3e6109c6d5664937e3300aa66634d1673731f48f
keleryohan <keler.yohan@hotmail.com>
2025-08-25 21:36:11 -0300

    feat: search post

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import './App.css';
+import SearchPost from './components/SearchPost'; //<< temporary
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/search" element={<SearchPost />} />
             </Routes>
           </main>
         </div>
