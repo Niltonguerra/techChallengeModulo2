24851f412c4d0d07a472d0126b8ffdae6082b7ef
Luigi-Martins <luigi.technics@gmail.com>
2025-09-17 20:22:42 -0300

    Refactoring using material ui and adjustment screen-login responsive

--- frontend-react/src/pages/LoginPage.tsx
+++ frontend-react/src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Container, Paper, Grid, Divider, Box } from '@mui/material';
 import LoginForm from '../components/Login/LoginForm';
 import RegisterBox from '../components/Login/RegisterBox';
 import type { User } from '../types/header-types';
-import '../components/Login/_LoginPage.scss';
 
 interface LoginPageProps {
   onLogin: (userData: User, token: string) => void;
@@ -10,12 +10,54 @@ interface LoginPageProps {
 
 const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
   return (
-    <main className="bg">
-      <div className="autentificacao">
-        <LoginForm onLogin={onLogin} />
-        <RegisterBox />
-      </div>
-    </main>
+    <Container
+      component="main"
+      maxWidth="md"
+      sx={{
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        py: 15,
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: { xs: 3, sm: 4, md: 6 },
+          width: '100%',
+          minHeight: '500px',
+          alignContent: 'center',
+        }}
+      >
+        <Grid
+          container
+          alignItems="flex-start"
+          justifyContent="center"
+          spacing={{ xs: 4, md: 2 }}
+        >
+          <Grid item xs={12} md={6}>
+            <LoginForm onLogin={onLogin} />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{
+                  display: { xs: 'none', md: 'block' },
+                  mx: { md: 2 },
+                  my: -5,
+                }}
+              />
+              <RegisterBox />
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 };
 
