b4f12bbec77c96683dd229ea94ca37fd2df45882
keleryohan <keler.yohan@hotmail.com>
2025-10-15 21:37:01 -0300

    reverting vite

--- back-end-educa-facil/src/polyfills.ts
+++ back-end-educa-facil/src/polyfills.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as nodeCrypto from 'crypto';
 if (!(globalThis as any).crypto) {
   (globalThis as any).crypto = {} as any;
