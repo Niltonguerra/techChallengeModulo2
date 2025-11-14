cbe5ab64e668be24b5f5772312ef5a87521213fa
Luis Augusto <luis50824@gmail.com>
2025-11-14 00:45:30 -0300

    fix: add header edit data

--- AppEducaFacil/app/edit-user-data.tsx
+++ AppEducaFacil/app/edit-user-data.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import UserForm from "@/components/UserForm";
+import Header from "@/components/header/header";
+import { View } from "react-native";
 
 export default function EditUserDataScreen() {
     const user = useSelector((state: RootState) => state.auth.user);
-    
+
     if (!user) return null;
 
     return (
-        <UserForm
-            userId={user.id}
-            userType={user.permission}
-        />
+        <View>
+            <Header title="Editar" />
+            <View style={{ marginBottom: 20 }} />
+            <UserForm userId={user.id} userType={user.permission} />
+        </View>
     );
 }
