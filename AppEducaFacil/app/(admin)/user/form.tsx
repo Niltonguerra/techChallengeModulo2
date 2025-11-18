5a42c75a98ac2d8c470bf3714c576be47f08abd8
Guilherme Pimentel <gui.pimentel2004@gmail.com>
2025-11-18 17:29:12 -0300

    correction router

--- AppEducaFacil/app/(admin)/user/form.tsx
+++ AppEducaFacil/app/(admin)/user/form.tsx
@@ -1,16 +1,17 @@
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import React from "react";
 import UserForm from "@/components/UserForm";
 
 export default function UserFormPage() {
   const { userId, userType } = useLocalSearchParams<{ userId?: string; userType?: string }>();
+  const router = useRouter();
 
   return (
     <UserForm
       userId={userId}
       userType={userType === "admin" ? "admin" : "user"}
       afterSubmit={() => {
-        history.back();
+        router.back(); 
       }}
     />
   );
