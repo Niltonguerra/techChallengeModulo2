fcff37f8e7d34bbb4a5f6e9f3d6f7f9d37da2949
Nilton Dionisio Guerra <niltondg.39@gmail.com>
2025-12-21 14:45:03 -0300

    ci:deploy do reset de senha

--- web/frontend-recuperar-senha/src/App.tsx
+++ web/frontend-recuperar-senha/src/App.tsx
@@ -12,32 +12,21 @@ import {
 import Footer from './components/Footer/Footer';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Header from './components/Header/Header';
-import SearchPost from './components/searchPost/SearchPost';
-import TypographyShowcase from './pages/styleGuide/TypographyShowcase';
 import './styles/scss/base/App.scss';
 import { theme } from './styles/scss/themes/theme';
-import type { User } from './types/header-types';
 import 'dayjs/locale/pt-br';
 import { store, type RootState } from './store';
-import { CreateEditPostFormPage } from './pages/createPostForm/CreatePostForm';
-import LoginPage from './pages/Login/LoginPage';
 import 'dayjs/locale/pt-br';
-import Admin from './pages/Admin/Admin';
 import SnackBarComponent from './components/Snackbar/Snackbar';
-import { loginSuccess, logout } from './store/userSlice';
-import { CreateEditUserFormPage } from './pages/createUserForm/CreateUserForm';
-import Home from './pages/home/Home';
+import { logout } from './store/userSlice';
 import ResetPassword from './pages/resetPassword/ResetPassword';
-import ForgotPasswordPage from './pages/ForgotPassword/ForgotPasswordForm';
+
 
 function App() {
   const dispatch = useDispatch();
   const { isLoggedIn, user } = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
 
-  const handleLogin = (userData: User, token: string) => {
-    dispatch(loginSuccess({ user: userData, token }));
-  };
 
   const handleLogout = () => {
     dispatch(logout());
